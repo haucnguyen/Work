c2e044cae02e49b25f65e2d2dff5ff874f0fbdf5
Hau Nguyen <Hau@airbears2-10-142-137-118.airbears2.1918.berkeley.edu>
2017-04-05 21:15:15 -0700

    hi

--- hw4/hw4/hash/OomageTestUtility.java
+++ hw4/hw4/hash/OomageTestUtility.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
 
+        int bucketNum = (oomages.hashCode() & 0x7FFFFFFF) % M;
 
         /* TODO: Write a utility function that returns true if the given oomages 
          * have hashCodes that would distribute them fairly evenly across
