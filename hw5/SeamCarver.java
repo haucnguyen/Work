6e375800e839aa3c3c6bccf25406338a7a1cf8ef
Hau Nguyen <Hau@airbears2-10-142-138-11.airbears2.1918.berkeley.edu>
2017-04-26 21:10:17 -0700

    hi

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -31,18 +31,17 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x >= width() || x < 0) {
+        if (x < 0 || x >= width()) {
             throw new IndexOutOfBoundsException("u mess up here fam");
+        }
+        if (y < 0 || y >= height()) {
+            throw new IndexOutOfBoundsException("u mess up here lol");
         }
         if (x == 0 || x == (width() - 1) || y == 0 || y == (height() - 1)) {
             return 255 * 255 * 3;
-        }
-        if (y >= height() || y < 0) {
-            throw new IndexOutOfBoundsException("u mess up here lol");
         } else {
             return gradientColorX(x, y) + gradientColorY(x, y);
         }
-
     }
 
 //    private double gradientColor(int x, int y) {
