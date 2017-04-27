5ad74f8ff34c6d1dbadd2c4a58176d12a3b7801e
Hau Nguyen <Hau@airbears2-10-142-137-131.airbears2.1918.berkeley.edu>
2017-04-26 17:46:46 -0700

    hi

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -5,9 +5,9 @@
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
-    public Picture picture;
-    public double[][] lol;
-    public int[][] poo;
+    private Picture picture;
+    private double[][] energys;
+    private int[][] poo;
 
     public SeamCarver(Picture picture) {
         this.picture = picture;
@@ -48,15 +48,75 @@ public class SeamCarver {
             return (Xred * Xred) + (Xblue * Xblue) * (Xgreen * Xgreen) + (Yred * Yred) + (Yblue * Yblue) + (Ygreen * Ygreen);
         }
     }
-
+    private void shortestPath(int x1, int y1, int x2, int y2) {
+        if (energys[x2][y2] > energy(x2, y2) + energys[x1][y1]) {
+            energys[x2][y2] = energy(x2, y2) + energys[x1][y1];
+            poo[x2][y2] = x1;
+        }
+    }
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        return null;
+        Picture original = picture;
+        Picture transpose = new Picture(original.height(), original.width());
+
+        for (int i = 0; i < transpose.height(); i++) {
+            for (int p = 0; p < transpose.width(); p++) {
+                transpose.set(i, p, original.get(p, i));
+            }
+        }
+        this.picture = transpose;
+        int[] lol = findVerticalSeam();
+        this.picture = original;
+        return lol;
     }
+
+
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        return null;
+        energys  = new double[width()][height()];
+        poo = new int[width()][height()];
+
+        for (int i = 0; i < width(); i++) {
+            for (int o = 0; i < height(); o++) {
+                energys[i][o] = Double.POSITIVE_INFINITY;
+            }
+        }
+
+        for (int i = 0; i < height(); i++) {
+            for (int l = 0; l < width(); l++) {
+                if (l > 0) {
+                    shortestPath(l, i, l - 1, i + 1);
+                }
+
+                shortestPath(l, i, l, i + 1);
+
+                if (l < width() -  1) {
+                    shortestPath(l, i, l + 1, i + 1);
+                }
+            }
+        }
+
+        double minEn = Double.POSITIVE_INFINITY;
+        int ooo = -1;
+        for (int i = 0; i < width(); i++) {
+            if (energys[i][height() - 1] < minEn) {
+                ooo = i;
+                minEn = energys[i][height() - 1];
+            }
+        }
+
+        assert ooo != -1;
+        int[] seam = new int[height()];
+        seam[height() - 1] = ooo;
+        int pre = poo[ooo][height() - 1];
+
+        for (int k = height() - 2; k >= 0; k--) {
+            seam[k] = pre;
+            pre = poo[pre][k];
+        }
+
+        return seam;
     }
 
     // remove horizontal seam from picture
