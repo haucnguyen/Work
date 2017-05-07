8ac107fcce051ed9923feb4a67a02ff7e4a2e653
Hau Nguyen <Hau@airbears2-10-142-136-181.airbears2.1918.berkeley.edu>
2017-05-06 23:23:08 -0700

    pls

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -2,4 +2,7 @@
  * Created by Hau on 5/6/17.
  */
 public class HuffmanDecoder {
+    public static void main(String[] args) {
+
+    }
 }
--- hw7/HuffmanEncoder.java
+++ hw7/HuffmanEncoder.java
@@ -1,5 +1,13 @@
+import java.util.Map;
+
 /**
  * Created by Hau on 5/6/17.
  */
 public class HuffmanEncoder {
+    public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
 }
